86c26df7965141cad643a6bbef76a93c2867be5a
Erjokul <kaerants@gmail.com>
2023-08-09 00:07:33 +0300

    First try

--- giftlyapp/views.py
+++ giftlyapp/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Account created for {username}!")
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/register.html", {"form": form})
+
+
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, f"You are now logged in as {user.username}")
+            return redirect("home")
+    else:
+        form = AuthenticationForm()
+    return render(request, "registration/login.html", {"form": form})
+
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, "You have been logged out")
+    return redirect("home")
+
+
+@login_required
+def home(request):
+    return render(request, "home.html")
+
+
+def all_products(request, pk):
+    gifts = Gift.all(), pk=pk
+
+
+def product_detail(request, pk):
+    prd = Gift.objects.get(id=id), pk=pk
