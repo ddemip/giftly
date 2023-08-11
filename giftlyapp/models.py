5bc631a2d47c0b151769f59d5c7202eb83e31863
Natalja <jakobsonnatalja@gmail.com>
2023-08-12 01:45:46 +0300

    Initial 2 models by Natalja

--- giftlyapp/models.py
+++ giftlyapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class User(models.Model):
+    user_id = models.IntegerField
+    email = models.CharField(max_lenght=255)
+    password = models.CharField(max_lenght=64)
+    first_name = models.CharField(max_lenght=255)
+    last_name = models.CharField(max_lenght=255)
+    role = models.CharField(max_lenght=10)
+
+    def __str__(self):
+        return self.user_id
+
+    def get_absolute_url(self):
+        return reverse("user_detail", args=[str(self.pk)])
+
+
+class Customer(models.Model):
+    customer_id = models.IntegerField
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    session_uuid = models.CharField(max_lenght=255)
+
+    def __str__(self):
+        return self.customer_id
+
+    def get_absolute_url(self):
+        return reverse("customer_detail", args=[str(self.pk)])
