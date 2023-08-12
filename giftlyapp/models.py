16220cff7291a24179e7672905e5caa1255fc6db
Natalja <jakobsonnatalja@gmail.com>
2023-08-12 16:20:48 +0300

    Added models

--- giftlyapp/models.py
+++ giftlyapp/models.py
@@ -1,45 +1,74 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 
-class User(models.Model):
-    email = models.CharField(max_lenght=255)
-    password = models.CharField(max_lenght=64)
-    first_name = models.CharField(max_lenght=255)
-    last_name = models.CharField(max_lenght=255)
-    role = models.CharField(max_lenght=10)
+# class GiftUser(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     email = models.CharField(max_length=255)
+#     password = models.CharField(max_length=64)
+#     first_name = models.CharField(max_length=255)
+#     last_name = models.CharField(max_length=255)
+#     role = models.CharField(max_length=10)
+#
+#     def __str__(self):
+#         return self.user
+#
+#     def get_absolute_url(self):
+#         return reverse("user_detail", args=[str(self.pk)])
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.user_id
-
-    def get_absolute_url(self):
-        return reverse("user_detail", args=[str(self.pk)])
-
+        return self.name
 
 class Customer(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    session_uuid = models.CharField(max_lenght=255)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    session_uuid = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.customer_id
+        return self.session_uuid
 
     def get_absolute_url(self):
         return reverse("customer_detail", args=[str(self.pk)])
 
-class Shopping_cart(models.Model):
-    customer_id = models.ForeignKey(User, on_delete=models.CASCADE)
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    thumbnail = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self):
-        return self.shopping_card_id
+        return self.title
 
-    def get_absolute_url(self):
-        return reverse("user_detail", args=[str(self.pk)])
 
+class ShoppingCart(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, null=True, on_delete=models.CASCADE)
+    product_quantity = models.IntegerField()
 
 class Order(models.Model):
-    customer_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    shopping_cart_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    recipient_email = models.CharField(max_lenght=255)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    shopping_cart_id = models.ForeignKey(ShoppingCart, on_delete=models.CASCADE)
+    recipient_email = models.CharField(max_length=255)
+    total_cost = models.DecimalField(max_digits=7, decimal_places=2)
+    date = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.total_cost
+
+
+class PaymentDetail(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    payment_method = models.CharField(max_length=64)
+    status = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.status
+
+
+
 
 
 
