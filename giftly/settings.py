6adee1e46502cbad87528142722f30bc2b9fa745
Demi Priivits <demi.priivits@gmail.com>
2023-09-03 01:06:14 +0300

    Template changes, small bugfixes everywhere

--- giftly/settings.py
+++ giftly/settings.py
@@ -143,3 +143,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+LANGUAGES = [
+    ('et', 'Estonian')
+]
+LANGUAGE_CODE = 'et'
